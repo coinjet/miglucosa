6419452205f2e82adb2313ca4f23d86d47bc5905
coinjet <micorreomty@gmail.com>
2025-03-25 00:16:34 -0600

    sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,25 @@
-const CACHE_NAME = "glucosa-app-v1";
-const urlsToCache = ["index.html", "styles.css", "app.js"];
+const CACHE_NAME = 'glucosa-app-cache-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json',
+  './assets/icon-192x192.png',
+  './assets/icon-512x512.png',
+  './assets/alarm.mp3'
+];
 
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
   );
 });
